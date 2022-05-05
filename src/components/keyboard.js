8039e8f05e42ce225dded0ee1fc3e4acc912274e
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2022-05-05 15:26:07 +0400

    feat: add discription

--- src/components/keyboard.js
+++ src/components/keyboard.js
@@ -8,6 +8,12 @@ class Keyboard {
         <h2 class="title">Virtual Keyboard</h2>
         <textarea id="screen"></textarea>
         <div id="keyboard"></div>
+        <p class="discription">Keyboard is created for Windows</p>
+        <p class="discription">Ctrl + Alt to change language</p>
+        <p class="discription">
+          If you press one Shift key second will be disabled.
+          I did this 'cos Windows don't trigger keyup event if both Shift Keys are pressed.
+         </p>
       </div>
     `;
 
