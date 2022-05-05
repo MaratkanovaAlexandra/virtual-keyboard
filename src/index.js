d878273b0a90cff4c064a7311d9c8cd0ac1e35aa
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2022-05-05 14:10:34 +0400

    feat: render keyboard

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import './style.css';
 
+import Keyboard from './components/keyboard';
+import Events from './components/events';
+
+const events = new Events();
+
+let lang = localStorage.getItem('lang');
+if (!lang) {
+  localStorage.setItem('lang', 'eng');
+  lang = 'eng';
+}
+
+Keyboard.init(document.body, lang);
