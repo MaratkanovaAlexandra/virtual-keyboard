ca1add31fbd4110fee22768b1671a46cbf3c1e67
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2022-05-05 14:09:23 +0400

    feat: add mouse events

--- src/components/events.js
+++ src/components/events.js
@@ -203,6 +203,58 @@ class Events {
       });
     });
   }
+
+  mousedown() {
+    window.addEventListener('mousedown', (e) => {
+      const key = e.target.closest('.key');
+      if (!key) return;
+      if (this.shidtKey === 'ShiftLeft' && key.classList.contains('ShiftRight')) return;
+      if (this.shidtKey === 'ShiftRight' && key.classList.contains('ShiftLeft')) return;
+      key.classList.add('active');
+
+      const keys = Array.from(document.querySelectorAll('.key'));
+      const screen = document.querySelector('#screen');
+      const index = screen.selectionEnd;
+
+      const dontPrintSpecialKey = this.specialKeyEvents(key.classList[1], key, keys);
+      const dontPrintAdditionalKey = this.additionalKeyEvents(key.classList[1], screen, index);
+
+      if (dontPrintSpecialKey || dontPrintAdditionalKey) return;
+
+      if ((key.classList.contains('ShiftLeft') || key.classList.contains('ShiftRight')) && !this.shift) this.shiftOn(key.classList[1]);
+
+      const letters = Array.from(key.children)
+        .filter((i) => i.classList.contains(this.lang))[0].children;
+      const letter = Array.from(letters).filter((i) => !i.classList.contains('hidden'))[0].innerHTML;
+
+      if (letter === 'Shift') return;
+
+      screen.value = `${screen.value.substring(0, index)}${letter[0]}${screen.value.substring(index)}`;
+      screen.selectionEnd = index + 1;
+    });
+  }
+
+  mouseup() {
+    window.addEventListener('mouseup', (e) => {
+      const key = e.target.closest('.key');
+      const keys = Array.from(document.querySelectorAll('.key'));
+
+      if (!key || key.classList.contains('CapsLock')) {
+        this.capsPress = false;
+        return;
+      }
+      if (key.classList.contains('ControlLeft') || key.classList.contains('ControlRight')) this.ctrl = false;
+      if (key.classList.contains('AltLeft') || key.classList.contains('AltRight')) this.alt = false;
+      if (key.classList.contains('ShiftLeft') || key.classList.contains('ShiftRight')) {
+        if (this.shidtKey === key.classList[1] && this.shift) this.shiftOff();
+      }
+
+      keys.forEach((i) => {
+        if (i.classList.contains('CapsLock')) return;
+        i.classList.remove('active');
+      });
+    });
+  }
 }
 
 export default Events;
